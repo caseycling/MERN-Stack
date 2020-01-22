85e52ddbd331fbc690366a8418f0386906a8b5f8
Casey <caseyclinga@gmail.com>
2020-01-22 12:02:08 -0800

    Finalized routes and backend

--- routes/api/items.js
+++ routes/api/items.js
@@ -7,11 +7,36 @@ const Item = require('../../models/Items');
 // @route   GET api/items
 // @desc    GET ALL items
 //@access   Public
-
 router.get('/', (req, res) => {
     Item.find()
     .sort({ date: -1 })
     .then(items => res.json(items))
 })
+
+// @route   POST api/items
+// @desc    Create an item
+//@access   Public
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    newItem.save().then(item => res.json(item))
+})
+
+// @route   DELETE api/items/:id
+// @desc    Delete an item
+//@access   Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+    .then(item => item.remove().then(() => { 
+        res.json({success: true})
+        console.log(`SUCCESS: ${req.body.name} deleted`)
+    }))
+    .catch(err =>  { 
+        res.status(404).json({success: false})
+        console.log('ERROR: Item does not exist')
+    });
+});
 
 module.exports = router;
