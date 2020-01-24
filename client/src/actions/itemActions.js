6a91d7453d32d61212e47ea1001f92623752d534
Casey <caseyclinga@gmail.com>
2020-01-23 20:50:00 -0800

    Created deleteItem function

---
+++ client/src/actions/itemActions.js
@@ -0,0 +1,17 @@
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types';
+
+export const getItems = () => {
+    return {
+        type: GET_ITEMS
+    };
+};
+//Delete item action
+//Takes id to find item to delete
+//Return returns to reducer (itemReducer.js)
+export const deleteItem = id => {
+    return {
+        type: DELETE_ITEM,
+        //QUESTION: What is payload?
+        payload: id
+    };
+};
