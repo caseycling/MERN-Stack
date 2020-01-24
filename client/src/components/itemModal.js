805410ea0cf452937dbef3c54a00efecdb2a6995
Casey <caseyclinga@gmail.com>
2020-01-23 21:51:43 -0800

    Succesfully implemented addItem functionality

--- client/src/components/itemModal.js
+++ client/src/components/itemModal.js
@@ -11,6 +11,7 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
+import uuid from 'uuid';
 
 class ItemModal extends Component {
 	state = {
@@ -26,6 +27,21 @@ class ItemModal extends Component {
 
 	onChange = e => {
 		this.setState({ [e.target.name]: e.target.value });
+	}
+
+	onSubmit = e => {
+		e.preventDefault();
+
+		const newItem = {
+			id: uuid(),
+			name: this.state.name
+		}
+
+		//Add item with addItem action
+		this.props.addItem(newItem);
+
+		//Close modal
+		this.toggle();
 	}
 
 	render() {
@@ -46,13 +62,19 @@ class ItemModal extends Component {
 						<Form onSubmit={this.onSubmit}>
 							<FormGroup>
 								<Label for="item">Item</Label>
-								<Input 
+								<Input
 									type="text"
 									name="name"
 									id="item"
 									placeholder="Add shopping item"
 									onChange={this.onChange}
 								/>
+								<Button
+									color="dark"
+									style={{ marginTop: '2rem' }}
+									block
+								>Add Item
+								</Button>
 							</FormGroup>
 						</Form>
 					</ModalBody>
@@ -62,4 +84,8 @@ class ItemModal extends Component {
 	}
 }
 
-export default connect()(ItemModal);
+const mapStateToProps = state => ({
+	item: state.item
+})
+
+export default connect(mapStateToProps, { addItem })(ItemModal);
