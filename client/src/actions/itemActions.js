c1e55a00db1e2fec9acf0c931f7f06a8d2f4fa98
Casey <caseyclinga@gmail.com>
2020-01-23 22:54:10 -0800

    Connected HTTP requests to back-end routes

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,32 +1,38 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING} from './types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
+import axios from 'axios';
 
-export const getItems = () => {
-    return {
-        type: GET_ITEMS
-    };
+export const getItems = () => dispatch => {
+	dispatch(setItemsLoading());
+	axios.get('/api/items').then(res =>
+		dispatch({
+			type: GET_ITEMS,
+			payload: res.data
+		})
+	)
+};
+//Add item action
+export const addItem = item => dispatch => {
+	axios.post('/api/items', item)
+		.then(res =>
+			dispatch({
+				type: ADD_ITEM,
+				payload: res.data
+			}))
 };
 //Delete item action
 //Takes id to find item to delete
 //Return returns to reducer (itemReducer.js)
-export const deleteItem = id => {
-    return {
-        type: DELETE_ITEM,
-        //QUESTION: What is payload?
-        payload: id
-    };
-};
-
-//Add item action
-export const addItem = item => {
-    return {
-        type: ADD_ITEM,
-        //QUESTION: What is payload?
-        payload: item
-    };
+export const deleteItem = id => dispatch => {
+	axios.delete(`/api/items/${id}`).then(res =>
+		dispatch({
+			type: DELETE_ITEM,
+			payload: id
+		})
+	)
 };
 
 export const setItemsLoading = () => {
-    return {
-        type: ITEMS_LOADING
-    }
+	return {
+		type: ITEMS_LOADING
+	}
 }
